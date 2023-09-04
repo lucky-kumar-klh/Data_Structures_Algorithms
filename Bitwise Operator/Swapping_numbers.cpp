87781b69a8f72429eb63b88ecc2480180bc93dc0
Luckyx100 <technicallucky861@gmail.com>
2023-09-04 14:15:25 +0530

    commit

---
+++ Bitwise Operator/Swapping_numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // WAP to swap 2 numbers using bitwise operators.
+    int a, b;  // a = 10, b = 8
+    cout << "Enter 2 numbers : ";
+    cin >> a >> b;
+    cout << "a = "<<a<< ", b = "<<b<<endl;
+    
+    // use XOR operator;
+    a = a^b; // a = 2
+    b = a^b; // b = 10
+    a = b^a; // a = 8
+    cout << "a = "<<a<< ", b = "<<b<<endl;
+
+    return 0;
+}
