3c3dd3b41c712010ac35a52cd8794e5dd928ece5
Luckyx100 <technicallucky861@gmail.com>
2023-09-23 19:29:35 +0530

    commit

--- xyz.cpp
+++ xyz.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-int main(){
-    int n = 5;
-    while ( n-- ){
-        cout<<"Hi lucky"<<endl;
-    }
-    return 0;
+
+int main() {
+	int T;
+	float L, V1, V2;    // v1 -> hair, v2 -> tortoise
+	// to win t1 < t2
+	// if t1 >= t2 --> print -1
+	//cin >> T;
+	//while ( T-- ){
+	    int t1, t2, diff;     // t1 -> hair, t2 -> tortoise
+	    cin >> L >> V1 >> V2;
+	    t1 = (ceil)(L/V1);
+	    t2 = (ceil)(L/V2);
+	    diff = t1-(t2+1);
+	    if ( diff >= 0 ) cout << diff << endl;
+	    else cout << "-1" << endl;
+	//}
+	return 0;
 }
