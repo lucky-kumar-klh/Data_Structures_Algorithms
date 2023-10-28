bb8da6acdbe8cf2b5de01611e51e831b65ddc8d3
Luckyx100 <technicallucky861@gmail.com>
2023-10-28 18:52:12 +0530

    commit

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-
+    vector<int> num = {-1,0,1,2,-1,-4};
+    vector<int> p;
+    for (int i = 0; i < num.size()-2; i++)
+    {
+        for (int j = i+1; j < num.size()-1; j++)
+        {
+            for (int k = j+1; k < num.size(); k++)
+            {
+                if ( num[i]+num[j]+num[k] == 0 )
+                {
+                    p.push_back(num[i]);
+                    p.push_back(num[j]);
+                    p.push_back(num[k]);
+                }
+            }
+        }
+    }
+    for ( int i = 0; i < p.size(); i++ ){
+        cout << p[i] << " ";
+    }
     return 0;
 }
