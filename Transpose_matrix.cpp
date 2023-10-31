a796ecabc1d477e3b2ab8a04f118a666cda48f8c
Luckyx100 <technicallucky861@gmail.com>
2023-10-31 23:52:00 +0530

    commit

---
+++ Transpose_matrix.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+void swaping(vector<vector<int>>& arr){
+    for (int i = 1; i < arr.size(); i++ ){
+        for (int j = 0; j < i; j++){
+            arr[j][i] = (arr[j][i]+arr[i][j]) - (arr[i][j] = arr[j][i]);
+            // OR
+            // arr[i][j] = arr[i][j] ^ arr[j][i];
+            // arr[j][i] = arr[i][j] ^ arr[j][i];
+            // arr[i][j] = arr[i][j] ^ arr[j][i];
+        } 
+    }
+}
+int main(){
+    vector<vector<int>> arr = {{1,1,1,1},
+                               {2,2,2,2},
+                               {3,3,3,3},
+                               {4,4,4,4}};
+    
+    swaping( arr );
+
+    for (int i = 0; i < arr.size(); i++){
+        for (int j = 0; j < arr.size(); j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
--- xyz.cpp
+++ xyz.cpp
@@ -1,32 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-	int k = 2;
-	vector<int> arr = {1,2,3,4};
-	int ans;
-    int count = 0, j = 0;
-    for (int i = 1; 1; i++){
-        if ( i != arr[j] )
-		{
-			ans = i;
-			count++;
-		}
-		else if ( j < arr.size()-1 ) j++;
-		if ( count == k ) break;
+void swaping(vector<vector<int>>& arr){
+    for (int i = 1; i < arr.size(); i++){
+        for (int j = 0; j < i; j++){
+            arr[i][j] = (arr[i][j]+arr[j][i]) - (arr[j][i] = arr[i][j]);
+        }
     }
-	printf("%d", ans);
+}
+int main() {
+    int temp;
+	vector<vector<int>> arr={{1,1,1,1},
+                             {2,2,2,2},
+                             {3,3,3,3},
+                             {4,4,4,4}};
+    swaping( arr );
+    for(int i = 0; i < arr.size(); i++){
+        for (int j = 0; j < arr.size(); j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
 	return 0;
 }
-/*
-if ( i < arr[j] ){
-            if ( i != arr[j] ){
-                count++;
-                ans = i;
-            } 
-            if ( count == k ){
-                break;
-            }
-            i++;
-        }
-        else  j++;
-*/
