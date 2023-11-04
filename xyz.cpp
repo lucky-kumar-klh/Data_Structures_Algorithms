0cde2069cd0569e4f8ca4ce34f949f8aadc7e24e
Luckyx100 <technicallucky861@gmail.com>
2023-11-04 21:42:13 +0530

    commit

--- xyz.cpp
+++ xyz.cpp
@@ -1,15 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-	int arr[8] = {1,2,3,4,5,6,7,8};
-	int n = 8;
-	for (int i = 1; i < 10; i++){
-		int count = 0;
-		for (int j = 0; j < 8; j++){
-			if ( i == arr[j] ) count++;
-		}
-		cout << i << " : " << count << " , ";
-	}
+
 	
+
 	return 0;
 }
