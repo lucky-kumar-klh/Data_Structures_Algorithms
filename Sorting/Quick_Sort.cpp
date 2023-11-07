7a7fc1cd561b483696eb7ce58d9be28ee9e27adb
Luckyx100 <technicallucky861@gmail.com>
2023-11-07 23:09:53 +0530

    commit

--- Sorting/Quick_Sort.cpp
+++ Sorting/Quick_Sort.cpp
@@ -13,8 +13,7 @@ int i=(low-1);
 for(int j=low;j<=high;j++)
 {
 	//If current element is smaller than the pivot
-	if(arr[j]<pivot)
-	{
+	if(arr[j]<pivot){
 	//Increment index of smaller element
 	i++;
 	swap(arr[i],arr[j]);
