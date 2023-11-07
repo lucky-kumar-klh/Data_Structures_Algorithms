c407beed535dcb723be2894dfdcd017eb95e855b
Luckyx100 <technicallucky861@gmail.com>
2023-11-07 23:32:20 +0530

    commit

--- Sorting/Insertion_Sort.cpp
+++ Sorting/Insertion_Sort.cpp
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+void swapArray(vector<int> &arr){
+    for (int i = 1; i < arr.size(); i++){
+        for (int j = i; j > 0; j--){
+            if ( arr[j] < arr[j-1] )
+                swap(arr[j], arr[j-1]);
+            else break;
+        }
+    }
+}
+void print(vector<int> &arr){
+    for (int i : arr)
+        cout << i << " ";
+}
 int main(){
 
-    
+    vector<int> arr = {10,8,1,2,5,0};
+    print(arr);
+
+    cout << endl;
+    swapArray(arr);
+
+    print(arr);
 
     return 0;
 }
