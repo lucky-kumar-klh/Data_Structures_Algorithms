96fc01c65e7acc0d7c7a5ceed8df070cd0dd6e32
Luckyx100 <technicallucky861@gmail.com>
2023-11-26 15:49:35 +0530

    commit

--- Basics/even_odd.cpp
+++ Basics/even_odd.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int num ;
-    //cout << "Enter any number : ";
-    cin >> num;
-    if ( num > 0 ) cout << num << " is a Positive number"<< endl;
-    else if ( num == 0 ) cout << num << " is a Nuetral number"<< endl;
-    else cout << num << " is a Negative number"<< endl;
+int main()
+{
+	int num;
+	cin >> num;
+	if (num > 0)
+
+		cout << num << " is a Positive number" << endl;
+	else if (num == 0)
+		cout << num << " is a Nuetral number" << endl;
+
+	else
+		cout << num << " is a Negative number" << endl;
 }
--- xyz.cpp
+++ xyz.cpp
@@ -2,12 +2,7 @@
 using namespace std;
 int main(){
 
-	vector<int> arr = {};
 
-	sort(arr.begin(), arr.end());
-
-	for ( auto i : arr )
-	    cout << i << " ";
-
+	
 	return 0;
 }
