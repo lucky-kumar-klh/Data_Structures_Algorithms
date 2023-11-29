e419ada759f8c4bcc17c0cd6b4765d1e4560d47b
Luckyx100 <technicallucky861@gmail.com>
2023-11-29 19:41:17 +0530

    commit

--- xyz.cpp
+++ xyz.cpp
@@ -1,22 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void BubbleSort(vector<int> &arr){
-	for (int i = 0; i < arr.size(); i++){
-		for (int j = 1; j < arr.size(); j++){
-			if (arr[j - 1] < arr[j])
-				swap(arr[j - 1], arr[j]);
-		}
-	}
-}
-
 int main(){
-	vector<int> arr = {1, 4, 2, 7, 5, 8};
-
-	BubbleSort(arr);
-
-	for (auto i : arr)
-		cout << i << " ";
-
+	
 	return 0;
 }
