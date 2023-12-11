9e1a491cb3a69b9ecee8a23120a53722397bd89c
Luckyx100 <technicallucky861@gmail.com>
2023-12-11 14:54:11 +0530

    commit

--- Sorting/Bubble_Sort.cpp
+++ Sorting/Bubble_Sort.cpp
@@ -1,32 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 void swapArray(vector<int>& arr){
-    for (int i = 0; i < arr.size(); i++){
-        bool swapping = 0;
-        for (int j = 0; j < arr.size()-1; j++){
-            if ( arr[j] > arr[j+1] ){
-                swap(arr[j], arr[j+1]);
-                // arr[j+1] = (arr[j]+arr[j+1]) - (arr[j]=arr[j+1]);
-                swapping = 1;
-            }
+  for (int i = 0; i < arr.size(); i++){
+    bool swapping = 0;
+      for (int j = 0; j < arr.size()-1; j++){
+        if ( arr[j] > arr[j+1] ){
+          swap(arr[j], arr[j+1]);
+          // arr[j+1] = (arr[j]+arr[j+1]) - (arr[j]=arr[j+1]);
+          swapping = 1;
         }
-        if ( swapping == 0 ) break;
-    }
+      }
+    if ( swapping == 0 ) break;
+  }
 }
 void print(vector<int>& arr){
-    for (int i : arr)
-        cout << i << " ";
+  for (auto i : arr)
+    cout << i << " ";
 }
+
 int main(){
 
-    vector<int> arr = {10,8,3,1,2,5};
+  vector<int> arr = {10,8,3,1,2,5};
 
-    print(arr);
+  print(arr);
 
-    cout << endl;
+  cout << endl;
 
-    swapArray(arr);
-    print(arr);
+  swapArray(arr);
+  print(arr);
 
-    return 0;
+  return 0;
 }
