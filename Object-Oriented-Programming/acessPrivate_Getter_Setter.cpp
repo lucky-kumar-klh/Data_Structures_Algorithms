30ba64839479ae39bcfba5e44c2d6cced338e125
Luckyx100 <technicallucky861@gmail.com>
2023-12-16 19:30:29 +0530

    commit

--- Object-Oriented-Programming/acessPrivate_Getter_Setter.cpp
+++ Object-Oriented-Programming/acessPrivate_Getter_Setter.cpp
@@ -1,7 +1,31 @@
+// To access private members of class we can use GETTER & SETTER concept
 #include <bits/stdc++.h>
 using namespace std;
+class hero{
 
+  private:
+
+  int health;
+  int attack;
+
+  // GETTER => 
+  int getHealth(){
+    health = 80;
+    return health;
+  }
+
+  public:
+
+  int age;
+};
 int main(){
   
+  hero ramesh;
+  ramesh.age = 18;    // public access modifier
+
+  cout << "Ramesh age : " << ramesh.age << endl;
+  // Using getter
+  cout << "Ramesh health : " << ramesh.getHealth() << endl;
+
   return 0;
 }
