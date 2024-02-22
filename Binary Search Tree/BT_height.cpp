a1c2ff7a942dc1454e0f22e1cc44d6cc6c043023
Luckyx100 <technicallucky861@gmail.com>
2024-02-23 00:18:16 +0530

    commit

--- Binary Search Tree/BT_height.cpp
+++ Binary Search Tree/BT_height.cpp
@@ -35,9 +35,50 @@ Node* createTree() {
     return root;
 }
 
+int height_of_tree(Node* Root){
+    int depth = 0;
+    if (Root == nullptr)
+        return depth;
+            
+    queue <Node*> q;
+    q.push(Root);
+    q.push(nullptr);
+    depth++;
+    while (!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+        if (temp == nullptr) {
+            if (!q.empty()) {
+                q.push(nullptr);
+                depth++;
+            }
+        }
+        else {
+            if (temp -> left != nullptr)
+                q.push(temp -> left);
+            if (temp -> right != nullptr)
+                q.push(temp -> right);
+        }
+    }
+    return depth;
+}
+
+int getTreeHeight(Node* root) {
+
+    if (root == nullptr)
+        return 0;
+
+    int left = getTreeHeight(root -> left);
+    int right = getTreeHeight(root -> right);
+
+    int ans = max(left, right) + 1;
+
+    return ans;
+}
+
 int main() {
 
     Node* root = createTree();
-    
+    cout << height_of_tree(root) << endl;
     return 0;
 }
