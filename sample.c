e3c1d0d3bedb05b8005f92980a19508eb0933884
Luckyx100 <technicallucky861@gmail.com>
2024-02-23 18:36:33 +0530

    commit

--- sample.c
+++ sample.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main(){
+    int n;
+    printf("Enter any number : ");
+    scanf("%d", &n);
 
-    
+    int min;
+    for (int i = 1; i < 2*n; i++){
+
+        for (int j = 1; j < 2*n; j++){
+
+            int a = i;
+            if (a > n) a = 2*n - i;
+            int b = j;
+            if (b > n) b = 2*n - j;
+            if (a < b) min = a;
+            else min = b;
+            printf("%d ", min);
+        }
+        printf("\n");
+    }
     return 0;
 }
