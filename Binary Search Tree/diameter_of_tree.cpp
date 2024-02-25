91ec77e39e7f561552d480f2543f146165d81f0a
Luckyx100 <technicallucky861@gmail.com>
2024-02-26 00:09:55 +0530

    commit

--- Binary Search Tree/diameter_of_tree.cpp
+++ Binary Search Tree/diameter_of_tree.cpp
@@ -35,17 +35,34 @@ Node* createTree() {
     return root;
 }
 
-int diameterOfTree(Node* root) {
+int height_of_tree(Node* root) {  // O(N)
+    if (root == nullptr)
+        return 0;
+
+    int left = height_of_tree(root -> left);
+    int right = height_of_tree(root -> right);
+
+    int ans = max(left, right) + 1;
+    return ans;
+}
+
+int diameterOfTree(Node* root) {  // O(N^2) :- O(diameter(height()))
 
     if (root == nullptr)
         return 0;
 
-    int left_max = 
+    int left_max = diameterOfTree(root -> left);
+    int right_max = diameterOfTree(root -> right);
+    int combo_max = height_of_tree(root -> left) + height_of_tree(root -> right) + 1;
+
+    int either_max = max(left_max, right_max);
+    int ans = (either_max, combo_max);
+    return ans;
 }
 
 int main() {
 
     Node* root = createTree();
-
+    cout << diameterOfTree(root) << endl;
     return 0;
 }
