541e2fb2e6bb97be843ccc5fdf4b7b0662df1e00
Luckyx100 <technicallucky861@gmail.com>
2024-03-05 18:33:10 +0530

    commit

--- Binary Search Tree/top_view.cpp
+++ Binary Search Tree/top_view.cpp
@@ -46,27 +46,33 @@ void find(Node* root, int pos, int &l, int &r) {
     find(root->right, pos+1, l, r);
 }
 
-void top_view_recursion(Node* root, int pos, vector<int> &ans, vector<int> &level, int l) {
-    if (!root)
+void top_view_recursion(Node* root, int pos, vector<int> &ans, vector<int> &level, int curr_level) {
+    if (root == nullptr)
         return;
 
-    if (level[pos] > l) {
+    if (curr_level < level[pos]) {
         ans[pos] = root -> data;
-        level[pos] = l;
+        level[pos] = curr_level;
     }
-    top_view_recursion(root->left, pos-1, ans, level, l+1);
-    top_view_recursion(root->right, pos+1, ans, level, l+1);
+    top_view_recursion(root->left, pos-1, ans, level, curr_level+1);
+    top_view_recursion(root->right, pos+1, ans, level, curr_level+1);
 }
 
 void using_recursion() {
     int l = 0, r = 0;
     // update l & r value
     find(root, 0, l, r);
-    
-    vector<int> ans(r-l+1);
-    vector<int> level(r-l+1, INT_MAX);
+    // using this find(), we will get l (left-most node) & r (right-most node)
 
-    top_view_recursion(root, -1*l, ans, level, 0);
+    vector<int> ans(r-l+1);
+
+    vector<int> level(r-l+1, INT_MAX);  // If 2 or more nodes are at same pos, then
+    // the minimum level will get the place, rest will be ignored
+
+    top_view_recursion(root, -1*l, ans, level, 0);  // update the ans vector
+    // -1*l will tell the index / pos of the root node, 
+    // lets say, root node's pos is 4, then :- 1,  2,  3,  4,  5,  6,  7.
+    //                                         l   l   l  root r   r   r
 
     return ans;
 }
