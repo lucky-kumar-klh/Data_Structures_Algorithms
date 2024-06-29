5dbe3fb3a027a8a6ee7afa210831df5467017992
Lucky Kumar <162823689+lucky-kumar-klh@users.noreply.github.com>
2024-06-29 19:31:58 +0530

    commit

--- Sorting/Insertion_Sort.cpp
+++ Sorting/Insertion_Sort.cpp
@@ -1,27 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-void swapArray(vector<int> &arr){
-    for (int i = 1; i < arr.size(); i++){
-        for (int j = i; j > 0; j--){
-            if ( arr[j] < arr[j-1] )
-                swap(arr[j], arr[j-1]);
-            else break;
-        }
+void swapArray(vector<int> &arr) {
+  for (int i = 1; i < arr.size(); i++) {
+    for (int j = i; j > 0; j--) {
+      if (arr[j] < arr[j - 1])
+        swap(arr[j], arr[j - 1]);
+      else
+        break;
     }
+  }
 }
-void print(vector<int> &arr){
-    for (int i : arr)
-        cout << i << " ";
+void print(vector<int> &arr) {
+  for (int i : arr)
+    cout << i << " ";
 }
-int main(){
+int main() {
 
-    vector<int> arr = {10,8,1,2,5,0};
-    print(arr);
+  vector<int> arr = {10, 8, 1, 2, 5, 0};
+  print(arr);
 
-    cout << endl;
-    swapArray(arr);
+  cout << endl;
+  swapArray(arr);
 
-    print(arr);
+  print(arr);
 
-    return 0;
+  return 0;
 }
